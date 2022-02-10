72192bae97ce3d2b1e2be2dc473d81c449d6403d
Linda Marko <lindamarko83@gmail.com>
2022-02-10 10:43:33 +0100

    Add views and router-links in Nav

---
+++ src/views/Navigation.vue
@@ -0,0 +1,18 @@
+<template>
+  <nav>
+    <router-link to="/meny">Meny</router-link>
+    <router-link to="/our-coffee">Vårt kaffe</router-link>
+    <router-link to="/profile">Min profil</router-link>
+    <router-link to="/orderstatus">OrderStatus</router-link>
+  </nav>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
