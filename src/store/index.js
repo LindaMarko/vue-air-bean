44cf46f807c2ca86483e7948c2ca1edac23364c6
anna.arlig <you@example.com>
2022-02-11 10:46:26 +0100

    add order history view with css and html, add testdata in store

--- src/store/index.js
+++ src/store/index.js
@@ -8,16 +8,29 @@ export default new Vuex.Store({
   state: {
     menu: [...menu],
     orders: [
-
-
       {
-        orderNumber: "",
-        orderDate: "",
-        totalSum: ""
-
-
+        orderNumber: "#AB1123282323Z",
+        orderDate: "20/03/06",
+        totalSum: 443
+      },
+      {
+        orderNumber: "#AB1128382323X",
+        orderDate: "20/03/03",
+        totalSum: 333
+      },
+      {
+        orderNumber: "#AB1444482323X",
+        orderDate: "20/02/21",
+        totalSum: 893
+      }
+    ],
+    users: [
+      {
+        name: "Sixten Kaffelövér",
+        epost: "sixten.kaffelover@zocom.se"
       }
     ]
+
 
   },
   mutations: {
